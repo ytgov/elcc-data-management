c4377cf61371ba7480cbb77c6110d0a2d630d0e7
Marlen Brunner <klondikemarlen@gmail.com>
2023-09-06 16:17:54 -0700

    :butterfly: Add funding submission line json table.

---
+++ api/src/db/migrations/2023.09.06T21.54.47.create-funding-submission-line-json-table.ts
@@ -0,0 +1,46 @@
+import { DataTypes } from "sequelize"
+
+import type { Migration } from "@/db/umzug"
+
+export const up: Migration = async ({ context: queryInterface }) => {
+  await queryInterface.createTable("funding_submission_line_json", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    centre_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "centres",
+        key: "id",
+      },
+    },
+    fiscal_year: {
+      type: DataTypes.STRING(10),
+      allowNull: false,
+    },
+    date_name: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    date_start: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    date_end: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    values: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+  })
+}
+
+export const down: Migration = async ({ context: queryInterface }) => {
+  await queryInterface.dropTable("funding_submission_line_json")
+}
