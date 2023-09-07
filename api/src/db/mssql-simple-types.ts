88a29615cd86462201302ca1f19ffba655a5d624
Marlen Brunner <klondikemarlen@gmail.com>
2023-09-07 14:14:15 -0700

    :pencil: Fix mssql type warning for Sequelize 7.

--- api/src/db/mssql-simple-types.ts
+++ api/src/db/mssql-simple-types.ts
@@ -5,7 +5,7 @@ const sequelizeVersion = (Sequelize as any).version
 const major = sequelizeVersion.split(".").map(Number)[0]
 
 if (major >= 7) {
-  console.warn("This was shim was probably make redundant in Sequelize v7, you should check!")
+  console.warn("This shim was probably made redundant in Sequelize v7, you should check!")
 }
 
 function DATETIME2(length?: number): string {
