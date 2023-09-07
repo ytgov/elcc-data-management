30a6f1cfb5fb7d82ccb2845b83c28880e5d04108
Marlen Brunner <klondikemarlen@gmail.com>
2023-09-06 17:26:31 -0700

    :recycle: Swap migration router over to using umzug for migrations.

--- api/src/routes/migration-router.ts
+++ api/src/routes/migration-router.ts
@@ -1,28 +1,38 @@
 import express, { type Request, type Response } from "express"
-import { db } from "../data"
-import { MigrationManager } from "../data/migration-manager"
-import { RequireRole } from "../middleware"
+import { db } from "@/data"
+import { MigrationManager } from "@/data/migration-manager"
+
+import { migrator } from "@/db/umzug"
 
 export const migrationRouter = express.Router()
 // migrationRouter.use(RequireRole("System Admin"));
 
-const migrator = new MigrationManager(db)
+const legacyMigrator = new MigrationManager(db)
+
+async function listMigrations() {
+  return Promise.all([migrator.executed(), migrator.pending()]).then(([executed, pending]) => {
+    return {
+      executed,
+      pending,
+    }
+  })
+}
 
 migrationRouter.get("/", async (req: Request, res: Response) => {
-  return res.json({ data: await migrator.listMigrations() })
+  return res.json({ data: await listMigrations() })
 })
 
 migrationRouter.get("/up", async (req: Request, res: Response) => {
-  await migrator.migrateUp()
-  return res.json({ data: await migrator.listMigrations() })
+  await migrator.up()
+  return res.json({ data: await listMigrations() })
 })
 
 migrationRouter.get("/down", async (req: Request, res: Response) => {
-  await migrator.migrateDown()
-  return res.json({ data: await migrator.listMigrations() })
+  await migrator.down()
+  return res.json({ data: await listMigrations() })
 })
 
 migrationRouter.get("/seed", async (req: Request, res: Response) => {
-  await migrator.seedUp()
+  await legacyMigrator.seedUp()
   return res.json({ data: "Seeding" })
 })
