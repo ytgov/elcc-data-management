0f812b4a31615511535dcb0af4dc954af37ed1f2
Marlen Brunner <klondikemarlen@gmail.com>
2023-09-11 12:40:01 -0700

    :factory: Add centre factory.

---
+++ api/tests/factories/centre-factories.ts
@@ -0,0 +1,47 @@
+import { Factory } from "fishery"
+import { faker } from "@faker-js/faker"
+
+import { Centre, CentreStatus } from "@/models"
+import { InferAttributes } from "sequelize"
+
+const yukonCommunities = [
+  "Carmacks",
+  "Dawson",
+  "Faro",
+  "Haines Junction",
+  "Mayo",
+  "Teslin",
+  "Watson Lake",
+  "Whitehorse",
+  "Ibex Valley",
+  "Mount Lorne",
+  "Carcross",
+  "Beaver Creek",
+  "Burwash Landing",
+  "Destruction Bay",
+  "Champagne",
+]
+
+export const centreFactory = Factory.define<InferAttributes<Centre>, never, Centre>(
+  ({ sequence, afterBuild, onCreate }) => {
+    afterBuild((attributes) => Centre.build(attributes))
+    onCreate((attribues) => Centre.create(attribues))
+
+    return {
+      id: sequence,
+      name: faker.person.firstName(),
+      license: faker.helpers.arrayElement([
+        `ECLC-${faker.number.int({ min: 100000, max: 999999 })}`,
+        null,
+      ]),
+      community: faker.helpers.arrayElement(yukonCommunities),
+      status: faker.helpers.objectValue(CentreStatus),
+      hotMeal: faker.helpers.arrayElement([true, false, null]),
+      licensedFor: faker.helpers.arrayElement([faker.number.int({ min: 1, max: 100 }), null]),
+      lastSubmission: faker.helpers.arrayElement([faker.date.recent(), null]),
+      createDate: faker.date.past(),
+    }
+  }
+)
+
+export default centreFactory
