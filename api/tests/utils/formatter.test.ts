68434e404fea0930b3ce151fbc53e93127cfd2f8
Marlen Brunner <klondikemarlen@gmail.com>
2023-09-11 11:16:36 -0700

    :recycle: Swap formatter to using standard number format pattern.
    Add some more tests.

--- api/tests/utils/formatter.test.ts
+++ api/tests/utils/formatter.test.ts
@@ -7,15 +7,23 @@ describe("api/src/utils/formatter.ts", () => {
       { input: 0.0, expected: "$0.00" },
       { input: -100, expected: "-$100.00" },
       { input: 10.5, expected: "$10.50" },
-      { input: 10.555, expected: "$10.55" }, // TODO: this should probably be $10.56
-      { input: Infinity, expected: "$0.00" },
+      { input: 10.555, expected: "$10.56" }, // TODO: this should probably be $10.56
       { input: NaN, expected: "$0.00" },
       { input: 0.005, expected: "$0.01" },
       { input: 0.004, expected: "$0.00" },
       { input: 0, expected: "$0.00" },
       { input: -0, expected: "$0.00" },
+      { input: undefined, expected: "$0.00" },
     ])("FormatDollar($input)", ({ input, expected }) => {
       expect(FormatDollar(input)).toBe(expected)
+    })
+
+    test("FormatDollar(Infinity)", () => {
+      expect(() => FormatDollar(Infinity)).toThrow("Infinity and -Infinity are not supported.")
+    })
+
+    test("FormatDollar(-Infinity)", () => {
+      expect(() => FormatDollar(-Infinity)).toThrow("Infinity and -Infinity are not supported.")
     })
   })
 })
