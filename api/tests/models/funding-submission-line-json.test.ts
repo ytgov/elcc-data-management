240ef69ef94ffe17a384d4ede0b9eb4f311dd73f
Marlen Brunner <klondikemarlen@gmail.com>
2023-09-11 11:30:52 -0700

    :white_check_mark: Add basic model tests for lines virtual attribute.

---
+++ api/tests/models/funding-submission-line-json.test.ts
@@ -0,0 +1,31 @@
+import { FundingSubmissionLineJson } from "@/models"
+
+describe("api/src/models/funding-submission-line-json.ts", () => {
+  describe("FundingSubmissionLineJson", () => {
+    describe("#lines", () => {
+      it("is #values parsed as json", () => {
+        const fundingSubmissionLineJson = FundingSubmissionLineJson.build({
+          dateName: "2020-01-01",
+          dateStart: new Date("2020-01-01"),
+          dateEnd: new Date("2020-01-01"),
+          fiscalYear: "2020",
+          values: "[1,2,3]",
+        })
+        expect(fundingSubmissionLineJson.lines).toEqual([1, 2, 3])
+      })
+
+      test("when #values is not valid json", () => {
+        const fundingSubmissionLineJson = FundingSubmissionLineJson.build({
+          dateName: "2020-01-01",
+          dateStart: new Date("2020-01-01"),
+          dateEnd: new Date("2020-01-01"),
+          fiscalYear: "2020",
+          values: "[NotValidJson]",
+        })
+        expect(() => fundingSubmissionLineJson.lines).toThrow(
+          "Unexpected token N in JSON at position 1"
+        )
+      })
+    })
+  })
+})
