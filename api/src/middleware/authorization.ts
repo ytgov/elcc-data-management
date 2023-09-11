1cb051c91a536438bf10099c1ca53e8658f0c07d
Marlen Brunner <klondikemarlen@gmail.com>
2023-09-11 09:03:27 -0700

    :beetle: Block types checks on some authorization code that doesn't show up to linting.

--- api/src/middleware/authorization.ts
+++ api/src/middleware/authorization.ts
@@ -2,14 +2,18 @@ import { type NextFunction, type Request, type Response } from "express"
 import { UserStatus } from "@/models"
 
 export function RequireAdmin(req: Request, res: Response, next: NextFunction) {
-  if (!req.user.isAdmin) {
+  // Linting is broken, this is necessary
+  // @ts-expect-error I can't figure out how to override express-jwt's req.user type definition
+  if (req.user?.isAdmin !== true) {
     return res.status(403).send("You aren't an admin")
   }
   next()
 }
 
 export function RequireActive(req: Request, res: Response, next: NextFunction) {
-  if (req.user.status != UserStatus.ACTIVE) return res.status(403).send("You aren't active")
+  // Linting is broken, this is necessary
+  // @ts-expect-error I can't figure out how to override express-jwt's req.user type definition
+  if (req.user?.status !== UserStatus.ACTIVE) return res.status(403).send("You aren't active")
 
   next()
 }
