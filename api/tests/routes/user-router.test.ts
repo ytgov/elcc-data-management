abed96139ac336941a78caa7f8dd59dbab6eaf31
Marlen Brunner <klondikemarlen@gmail.com>
2023-09-12 16:54:01 -0700

    :heavy_check_mark: Simply setup, and mock inline for now.
    I'll figure out how to abstract the authorization mocking another time.

--- api/tests/routes/user-router.test.ts
+++ api/tests/routes/user-router.test.ts
@@ -2,28 +2,33 @@ import request from "supertest"
 import { Request, Response, NextFunction } from "express"
 
 import app from "@/app"
-// import mockCurrentUser from "@/support/mock-current-user"
+import { userFactory } from "@/factories"
 
-const userData = { id: 1, name: "John Doe" }
+let user: any
 jest.mock("@/middleware/authz.middleware", () => ({
   checkJwt: (req: Request, res: Response, next: NextFunction) => next(),
   autheticateAndLoadUser: (req: Request, res: Response, next: NextFunction) => {
-    req.user = userData // Mock user data
+    req.user = user // Mock user data
     next()
   },
 }))
 
 describe("api/src/routes/user-router.ts", () => {
+  beforeEach(() => {
+    user = null
+  })
+
   describe("GET /api/user/me", () => {
     test("when authenticated returns the current user", async () => {
-      // mockCurrentUser(user)
+      user = userFactory.build()
 
       return request(app)
         .get("/api/user/me")
         .expect("Content-Type", /json/)
-        .expect(200, { data: userData })
+        .expect(200, { data: JSON.parse(JSON.stringify(user.dataValues)) })
     })
 
-    test("when not authenticated returns 401", async () => {})
+    test("when not checkJwt failes", async () => {})
+    test("when autheticateAndLoadUser failes", async () => {})
   })
 })
