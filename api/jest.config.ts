1a678595e74c59bfe711cf2c2aff894e070d4064
Marlen Brunner <klondikemarlen@gmail.com>
2023-09-11 21:44:10 -0700

    :gear: Add database setup functions for jest.
    NOTE: you don't need to kill the sequelize client in a globalTeardown, that happens on process.exit.
    Also, if you do close the connection, test-reruns will break.
    
    I haven't figured out how to only run database cleanup on specific flagged tests, but it shouldn't be too hard to do later.

--- api/jest.config.ts
+++ api/jest.config.ts
@@ -6,6 +6,8 @@ export const jestConfig: JestConfigWithTsJest = {
   moduleNameMapper: {
     "^@/(.*)$": ["<rootDir>/src/$1", "<rootDir>/tests/$1"],
   },
+  setupFilesAfterEnv: ["<rootDir>/tests/setup.ts"], // Runs once for each test file
+  globalSetup: "<rootDir>/tests/global-setup.ts", // Runs once before all tests
 }
 
 export default jestConfig
--- api/src/models/index.ts
+++ api/src/models/index.ts
@@ -1,3 +1,5 @@
+import db from "@/db/db-client"
+
 import Centre from "@/models/centre"
 import CentreFundingPeriod from "@/models/centre-funding-period"
 import FundingSubmissionLine from "@/models/funding-submission-line"
@@ -25,9 +27,5 @@ export * from "@/models/log"
 export * from "@/models/user-role"
 export * from "@/models/user"
 
-// I want to keep default exports to encourage the "single thing per file" principle
-// you can still access the model bundle via import * as models from "@/models"
-// but import models from "@/models" will be undefined
-// Its not ideal, but it's hopefully the best of both worlds?
-// Avoids leaking random last default export from model files
-export default undefined
+// special db instance that has access to all models.
+export default db
