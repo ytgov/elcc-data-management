3319b43dad7790dc2ba56eecfaec430ef9b02489
Marlen Brunner <klondikemarlen@gmail.com>
2023-09-12 16:37:08 -0700

    :construction: auth mocking fixed but need to externalize the mock.

--- api/tests/routes/user-router.test.ts
+++ api/tests/routes/user-router.test.ts
@@ -1,26 +1,27 @@
 import request from "supertest"
+import { Request, Response, NextFunction } from "express"
 
 import app from "@/app"
-import mockCurrentUser from "@/support/mock-current-user"
-import { userFactory } from "@/factories"
+// import mockCurrentUser from "@/support/mock-current-user"
+
+const userData = { id: 1, name: "John Doe" }
+jest.mock("@/middleware/authz.middleware", () => ({
+  checkJwt: (req: Request, res: Response, next: NextFunction) => next(),
+  autheticateAndLoadUser: (req: Request, res: Response, next: NextFunction) => {
+    req.user = userData // Mock user data
+    next()
+  },
+}))
 
 describe("api/src/routes/user-router.ts", () => {
   describe("GET /api/user/me", () => {
     test("when authenticated returns the current user", async () => {
-      const user = await userFactory.create()
+      // mockCurrentUser(user)
 
-      mockCurrentUser(user)
-
-      request(app)
+      return request(app)
         .get("/api/user/me")
         .expect("Content-Type", /json/)
-        .expect(200, user.dataValues)
-        .end((error: any, response: any) => {
-          console.log(response.status)
-          console.log(response.body)
-          // console.log(response)
-          if (error) throw error
-        })
+        .expect(200, { data: userData })
     })
 
     test("when not authenticated returns 401", async () => {})
--- api/tests/support/mock-current-user.ts
+++ api/tests/support/mock-current-user.ts
@@ -5,9 +5,11 @@ import { User } from "@/models"
 export function mockCurrentUser(user: User) {
   jest.mock("@/middleware/authz.middleware", () => ({
     checkJwt: (req: Request, res: Response, next: NextFunction) => {
+      console.log("mocked checkJwt")
       next()
     },
     authenticateAndLoadUser: (req: Request, res: Response, next: NextFunction) => {
+      console.log("mocked authenticateAndLoadUser")
       req.user = user
       next()
     },
