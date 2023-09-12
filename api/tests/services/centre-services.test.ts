856b70f48f590bba6f660f26686f3a92e20740bd
Marlen Brunner <klondikemarlen@gmail.com>
2023-09-11 22:07:10 -0700

    :construction: Work in progress centre service test.
    Reveals several major flaws in current architecture.
    1) object creation seems to require and id
    2) queries against date objects aren't what you would expect.
    3) its hard to conveniently generate model attributes via factories.

--- api/tests/services/centre-services.test.ts
+++ api/tests/services/centre-services.test.ts
@@ -1,4 +1,4 @@
-import { Centre } from "@/models"
+import { Centre, Log } from "@/models"
 import { CentreServices } from "@/services"
 
 import { centreFactory, userFactory } from "@/factories"
@@ -6,14 +6,43 @@ import { centreFactory, userFactory } from "@/factories"
 describe("api/src/services/centre-services.ts", () => {
   describe("CentreServices", () => {
     describe(".create", () => {
-      test("creates a new centre in the database", () => {
-        expect.assertions(2)
+      test("creates a new centre in the database", async () => {
         const currentUser = userFactory.build()
-        const centerAttributes = centreFactory.build()
-        return CentreServices.create(centerAttributes, { currentUser }).then((centre) => {
-          expect(centre).toBeInstanceOf(Centre)
-          expect(centre).toMatchObject(centerAttributes)
-        })
+        // const { dataValues: attributes } = centreFactory.build()
+
+        const attributes = {
+          id: 1,
+          name: "Reba",
+          license: "ECLC-438361",
+          community: "Destruction Bay",
+          status: "Up to date",
+          hotMeal: false,
+          licensedFor: null,
+          lastSubmission: new Date("2023-09-11"),
+          createDate: new Date("2023-07-08T00:08:18.953Z"),
+        }
+
+        console.log("attributes", JSON.stringify(attributes, null, 2))
+
+        expect(await Centre.count()).toBe(0)
+
+        const centre = await CentreServices.create(attributes, { currentUser })
+
+        expect(centre).toBeInstanceOf(Centre)
+        expect(
+          await Centre.count({
+            where: {
+              name: "Reba",
+              license: "ECLC-438361",
+              community: "Destruction Bay",
+              status: "Up to date",
+              hotMeal: false,
+              licensedFor: null,
+              // lastSubmission: new Date("2023-09-11"),
+              // createDate: new Date("2023-07-08T00:08:18.000Z"),
+            },
+          })
+        ).toBe(1)
       })
     })
   })
