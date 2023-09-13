3fc536c4b00c0b11c367e846732c4c0d613fbda3
Marlen Brunner <klondikemarlen@gmail.com>
2023-09-12 18:17:22 -0700

    :beetle: Fix type errors in sample migration.
    These prevented compilation, so I updated the sample to show some examples of table creation and threw and error by default.
    
    Throwing an error will prevent the migration from running until the developer actually implements something.

--- api/src/db/templates/sample-migration.ts
+++ api/src/db/templates/sample-migration.ts
@@ -1,11 +1,40 @@
 import { DataTypes } from "sequelize"
 
 import type { Migration } from "@/db/umzug"
+import { MssqlSimpleTypes } from "@/db/mssql-simple-types"
 
 export const up: Migration = async ({ context: queryInterface }) => {
-  await queryInterface.query(`raise fail('up migration not implemented')`)
+  throw new Error("Not implemented")
+  await queryInterface.createTable("users", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    first_name: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    is_admin: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+    createdAt: {
+      type: MssqlSimpleTypes.DATETIME2(0),
+      allowNull: false,
+      defaultValue: MssqlSimpleTypes.NOW,
+    },
+    updatedAt: {
+      type: MssqlSimpleTypes.DATETIME2(0),
+      allowNull: false,
+      defaultValue: MssqlSimpleTypes.NOW,
+    },
+  })
 }
 
 export const down: Migration = async ({ context: queryInterface }) => {
-  await queryInterface.query(`raise fail('down migration not implemented')`)
+  throw new Error("Not implemented")
+  await queryInterface.dropTable("users")
 }
