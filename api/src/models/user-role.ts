9748e1a57961583958535fb418174ad73790e4b8
Marlen Brunner <klondikemarlen@gmail.com>
2023-09-25 18:43:11 -0700

    :beetle: Stop passing foreignKey option to user roles belongs to.
    I'm pretty sure this is the wrong option? Either that or I should be specifying it as `user_id` or something?

--- api/src/models/user-role.ts
+++ api/src/models/user-role.ts
@@ -43,9 +43,7 @@ export class UserRole extends Model<InferAttributes<UserRole>, InferCreationAttr
   }
 
   static establishAssociations() {
-    this.belongsTo(User, {
-      foreignKey: "userId",
-    })
+    this.belongsTo(User)
   }
 }
 
