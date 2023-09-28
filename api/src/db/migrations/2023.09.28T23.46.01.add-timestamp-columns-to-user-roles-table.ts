eb96b62fffa15d8c8c3ebd2172bf068cc61eb24b
Marlen Brunner <klondikemarlen@gmail.com>
2023-09-28 16:47:09 -0700

    :butterfly: Add timestamp columns to the user_roles table.

---
+++ api/src/db/migrations/2023.09.28T23.46.01.add-timestamp-columns-to-user-roles-table.ts
@@ -0,0 +1,20 @@
+import type { Migration } from "@/db/umzug"
+import { MssqlSimpleTypes } from "@/db/utils/mssql-simple-types"
+
+export const up: Migration = async ({ context: queryInterface }) => {
+  await queryInterface.addColumn("user_roles", "created_at", {
+    type: MssqlSimpleTypes.DATETIME2(),
+    allowNull: false,
+    defaultValue: MssqlSimpleTypes.NOW,
+  })
+  await queryInterface.addColumn("user_roles", "updated_at", {
+    type: MssqlSimpleTypes.DATETIME2(),
+    allowNull: false,
+    defaultValue: MssqlSimpleTypes.NOW,
+  })
+}
+
+export const down: Migration = async ({ context: queryInterface }) => {
+  await queryInterface.removeColumn("user_roles", "created_at")
+  await queryInterface.removeColumn("user_roles", "updated_at")
+}
