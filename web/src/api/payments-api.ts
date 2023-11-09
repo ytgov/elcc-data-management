399f841c51e8f5fb39954586586cf7024daf233c
Marlen Brunner <klondikemarlen@gmail.com>
2023-11-09 11:57:14 -0700

    :label: Adding types to payments api.

--- web/src/api/payments-api.ts
+++ web/src/api/payments-api.ts
@@ -3,6 +3,7 @@ import http from "@/api/http-client"
 export type Payment = {
   id: number
   centreId: number
+  fiscalYear: string
   name: string
   amountInCents: number
   paidOn: string
@@ -16,18 +17,20 @@ export const paymentsApi = {
     page,
     perPage,
     ...otherParams
-  }: { where?: any; page?: number; perPage?: number; otherParams?: any } = {}) {
+  }: { where?: any; page?: number; perPage?: number; otherParams?: any } = {}): Promise<{
+    payments: Payment[]
+  }> {
     return http
       .get("/api/payments", { params: { where, page, perPage, ...otherParams } })
       .then(({ data }) => data)
   },
-  create(attributes: any) {
+  create(attributes: Partial<Payment>): Promise<{ payment: Payment }> {
     return http.post("/api/payments", attributes).then(({ data }) => data)
   },
-  update(paymentId: number, attributes: any) {
+  update(paymentId: number, attributes: any): Promise<{ payment: Payment }> {
     return http.patch(`/api/payments/${paymentId}`, attributes).then(({ data }) => data)
   },
-  delete(paymentId: number) {
+  delete(paymentId: number): Promise<void> {
     return http.delete(`/api/payments/${paymentId}`).then(({ data }) => data)
   },
 }
