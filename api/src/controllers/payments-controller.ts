d1c584af70204400a34f4a71fab3737d6f03a733
Marlen Brunner <klondikemarlen@gmail.com>
2023-11-09 11:59:21 -0700

    :construction: Add full create/read/update/delete to payments controller.
    TODO: add in permitted params and policy checks.

--- api/src/controllers/payments-controller.ts
+++ api/src/controllers/payments-controller.ts
@@ -1,3 +1,4 @@
+import { isNil } from "lodash"
 import { WhereOptions } from "sequelize"
 
 import BaseController from "./base-controller"
@@ -10,7 +11,51 @@ export class PaymentsController extends BaseController {
     return Payment.findAll({
       where,
       order: ["paidOn"],
-    }).then((payments) => {})
+    }).then((payments) => {
+      return this.response.json({ payments })
+    })
+  }
+
+  async create() {
+    return Payment.create(this.request.body)
+      .then((payment) => {
+        return this.response.status(201).json({ payment })
+      })
+      .catch((error) => {
+        return this.response.status(422).json({ message: `Payment creation failed: ${error}` })
+      })
+  }
+
+  async update() {
+    const payment = await this.loadPayment()
+    if (isNil(payment)) return this.response.status(404).json({ message: "Payment not found." })
+
+    return payment
+      .update(this.request.body)
+      .then((payment) => {
+        return this.response.json({ payment })
+      })
+      .catch((error) => {
+        return this.response.status(422).json({ message: `Payment update failed: ${error}` })
+      })
+  }
+
+  async destroy() {
+    const payment = await this.loadPayment()
+    if (isNil(payment)) return this.response.status(404).json({ message: "Payment not found." })
+
+    return payment
+      .destroy()
+      .then(() => {
+        return this.response.status(204).end()
+      })
+      .catch((error) => {
+        return this.response.status(422).json({ message: `Payment deletion failed: ${error}` })
+      })
+  }
+
+  private loadPayment(): Promise<Payment | null> {
+    return Payment.findByPk(this.params.paymentId)
   }
 }
 
