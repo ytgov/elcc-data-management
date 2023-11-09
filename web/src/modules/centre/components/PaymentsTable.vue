7b0922a368f4b059336a349a8aa59f64ffa331cc
Marlen Brunner <klondikemarlen@gmail.com>
2023-11-08 19:14:22 -0700

    :construction: Begin building out add payment interface.

--- web/src/modules/centre/components/PaymentsTable.vue
+++ web/src/modules/centre/components/PaymentsTable.vue
@@ -16,20 +16,68 @@
         <td>{{ payment.amountInCents }}</td>
         <td>{{ payment.paidAt }}</td>
       </tr>
+      <tr v-if="payments.length === 0">
+        <td
+          colspan="3"
+          class="text-center"
+        >
+          No payments found
+        </td>
+      </tr>
+      <tr v-if="!isEmpty(newPayment)">
+        <td>
+          <v-text-field
+            v-model="newPayment.title"
+            label="Payment Name"
+            density="compact"
+            hide-details
+          ></v-text-field>
+        </td>
+        <td>
+          <v-text-field
+            v-model="newPayment.amountInCents"
+            label="Payment Amount"
+            density="compact"
+            hide-details
+          ></v-text-field>
+        </td>
+        <td>
+          <v-text-field
+            v-model="newPayment.paidAt"
+            label="Paid At"
+            density="compact"
+            hide-details
+          ></v-text-field>
+        </td>
+      </tr>
     </tbody>
   </v-table>
+
+  <div class="mt-4 mr-4 d-flex justify-end">
+    <v-btn
+      color="primary"
+      @click="addRow"
+      >Add row</v-btn
+    >
+  </div>
 </template>
 
 <script lang="ts" setup>
 import { ref, onMounted } from "vue"
+import { isEmpty } from "lodash"
 
 import paymentsApi from "@/api/payments-api"
 
 const payments = ref([])
+const newPayment = ref({})
 
 onMounted(async () => {
   await paymentsApi.list().then(({ payments: newPayments }) => {
     payments.value = newPayments
   })
 })
+
+function addRow() {
+  newPayment.value.id = -1
+}
 </script>
