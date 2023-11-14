44fbb568e6898e5e48670c703e965ef7e1290bcd
Marlen Brunner <klondikemarlen@gmail.com>
2023-11-14 11:38:24 -0700

    :label: Add types to dateRule validator.

--- web/src/utils/validators.ts
+++ web/src/utils/validators.ts
@@ -1,4 +1,4 @@
-export function dateRule(value) {
+export function dateRule(value: string) {
   const pattern = /^\d{4}-\d{2}-\d{2}$/
   return pattern.test(value) || "Date must be in yyyy-mm-dd format"
 }
