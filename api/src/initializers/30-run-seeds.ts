ab6220dbf2c6303be395c11b10a3934890ff5ae1
Marlen Brunner <klondikemarlen@gmail.com>
2023-12-07 10:26:15 -0700

    :hammer: Add environment flag to skip seeding.

--- api/src/initializers/30-run-seeds.ts
+++ api/src/initializers/30-run-seeds.ts
@@ -1,6 +1,15 @@
 import { seeder } from "@/db/umzug"
+import { Centre } from "@/models"
 
 export async function runSeeds(): Promise<void> {
+  if (process.env.SKIP_SEEDING_UNLESS_EMPTY === "true") {
+    const count = await Centre.count()
+    if (count > 0) {
+      console.log("Skipping seeding as SKIP_SEEDING_UNLESS_EMPTY set, and data already seeded.")
+      return
+    }
+  }
+
   await seeder.up()
   return
 }
