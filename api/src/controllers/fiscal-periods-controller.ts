d402a519ddb0365464d5af79cdab256479ae144f
Marlen Brunner <klondikemarlen@gmail.com>
2023-12-11 16:13:03 -0700

    :sparkles: add back-end routing for fiscal periods.

---
+++ api/src/controllers/fiscal-periods-controller.ts
@@ -0,0 +1,26 @@
+import { WhereOptions } from "sequelize"
+
+import BaseController from "./base-controller"
+
+import { FiscalPeriod } from "@/models"
+
+export class FiscalPeriodsController extends BaseController {
+  async index() {
+    const where = this.query.where as WhereOptions<FiscalPeriod>
+    try {
+      const fiscalPeriods = await FiscalPeriod.findAll({
+        where,
+        order: ["dateStart", "dateEnd"],
+      })
+      return this.response.json({
+        fiscalPeriods,
+      })
+    } catch (error) {
+      return this.response
+        .status(400)
+        .json({ message: `Invalid query for fiscalPeriods: ${error}` })
+    }
+  }
+}
+
+export default FiscalPeriodsController
--- api/src/controllers/index.ts
+++ api/src/controllers/index.ts
@@ -1,3 +1,4 @@
+export { FiscalPeriodsController } from "./fiscal-periods-controller"
 export * from "./funding-submission-line-jsons-controller"
 export * from "./payments-controller"
 
