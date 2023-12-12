3931eeb1d3ba2e4761f67f7ea8f13c6f6f3891c5
Marlen Brunner <klondikemarlen@gmail.com>
2023-12-12 11:34:36 -0700

    :butterfly: Add employee benefits table.
    Why? To handle divergence of employ benefits from funding_submission_line_jsons. Employee benefits are not per-child. See https://yg-hpw.atlassian.net/browse/ELCC-27

---
+++ api/src/db/migrations/2023.12.11T17.00.27.create-employee-benefits-table.ts
@@ -0,0 +1,73 @@
+import { DataTypes } from "sequelize"
+
+import type { Migration } from "@/db/umzug"
+import { MssqlSimpleTypes } from "@/db/utils/mssql-simple-types"
+
+export const up: Migration = async ({ context: queryInterface }) => {
+  await queryInterface.createTable("employee_benefits", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    centre_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "centres", // use real table name here
+        key: "id",
+      },
+    },
+    fislcal_period_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "fiscal_periods", // use real table name here
+        key: "id",
+      },
+    },
+    gross_payroll_monthly_actual: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+    },
+    gross_payroll_monthly_estimated: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+    },
+    cost_cap_percentage: {
+      type: DataTypes.DECIMAL(5, 2),
+      allowNull: false,
+    },
+    employee_cost_actual: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+    },
+    employee_cost_estimated: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+    },
+    employer_cost_actual: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+    },
+    employer_cost_estimated: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+    },
+    created_at: {
+      type: MssqlSimpleTypes.DATETIME2(0),
+      allowNull: false,
+      defaultValue: MssqlSimpleTypes.NOW,
+    },
+    updated_at: {
+      type: MssqlSimpleTypes.DATETIME2(0),
+      allowNull: false,
+      defaultValue: MssqlSimpleTypes.NOW,
+    },
+  })
+}
+
+export const down: Migration = async ({ context: queryInterface }) => {
+  await queryInterface.dropTable("employee_benefits")
+}
