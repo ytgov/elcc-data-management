7caacdcf6b717c69584827e26dde1b275531b411
Marlen Brunner <klondikemarlen@gmail.com>
2023-12-12 09:20:56 -0700

    :fire: Delete fiscal period api actions that don't have back-end code for them yet.

--- web/src/api/fiscal-periods-api.ts
+++ web/src/api/fiscal-periods-api.ts
@@ -28,18 +28,6 @@ export const fiscalPeriodsApi = {
   }> {
     return http.get("/api/fiscal-periods", { params }).then(({ data }) => data)
   },
-  get(fiscalPeriodId: number): Promise<{ fiscalPeriod: FiscalPeriod }> {
-    return http.get(`/api/fiscal-periods/${fiscalPeriodId}`).then(({ data }) => data)
-  },
-  create(attributes: Partial<FiscalPeriod>): Promise<{ fiscalPeriod: FiscalPeriod }> {
-    return http.post("/api/fiscal-periods", attributes).then(({ data }) => data)
-  },
-  update(fiscalPeriodId: number, attributes: any): Promise<{ fiscalPeriod: FiscalPeriod }> {
-    return http.patch(`/api/fiscal-periods/${fiscalPeriodId}`, attributes).then(({ data }) => data)
-  },
-  delete(fiscalPeriodId: number): Promise<void> {
-    return http.delete(`/api/fiscal-periods/${fiscalPeriodId}`).then(({ data }) => data)
-  },
 }
 
 export default fiscalPeriodsApi
