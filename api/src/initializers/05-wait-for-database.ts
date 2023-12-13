4cc815791428c3cb6cd113cd449eab04dc63e7fe
Marlen Brunner <klondikemarlen@gmail.com>
2023-12-13 12:13:20 -0700

    :recycle: Drop ability to connect to master database in production.
    We don't have access to the master database credentials, so this health check will use the regular credentials in production.

--- api/src/initializers/05-wait-for-database.ts
+++ api/src/initializers/05-wait-for-database.ts
@@ -1,23 +1,12 @@
-import { DB_HOST, DB_PASS, DB_PORT, NODE_ENV } from "@/config"
+import { DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER, NODE_ENV } from "@/config"
 import { Sequelize } from "sequelize"
-
-const db = new Sequelize({
-  dialect: "mssql",
-  username: "sa", // default user that should always exist
-  database: "master", // default database that should always exist
-  password: DB_PASS,
-  host: DB_HOST,
-  port: DB_PORT,
-  schema: "dbo",
-  logging: NODE_ENV === "development" ? console.log : false,
-})
 
 const INTERVAL_SECONDS = 5
 const TIMEOUT_SECONDS = 5
 const RETRIES = 3
 const START_PERIOD_SECONDS = 10
 
-function checkHealth(timeoutSeconds: number) {
+function checkHealth(db: Sequelize, timeoutSeconds: number) {
   return new Promise((resolve, reject) => {
     const timer = setTimeout(() => reject(new Error("Connection timeout")), timeoutSeconds * 1000)
     db.authenticate()
@@ -44,11 +33,32 @@ export async function waitForDatabase({
   retries?: number
   startPeriodSeconds?: number
 } = {}): Promise<void> {
+  let username: string
+  let database: string
+  if (NODE_ENV === "production") {
+    username = DB_USER
+    database = DB_NAME
+  } else {
+    username = "sa" // default user that should always exist
+    database = "master" // default database that should always exist
+  }
+
+  const db = new Sequelize({
+    dialect: "mssql",
+    username,
+    database,
+    password: DB_PASS,
+    host: DB_HOST,
+    port: DB_PORT,
+    schema: "dbo",
+    logging: NODE_ENV === "development" ? console.log : false,
+  })
+
   await sleep(startPeriodSeconds)
 
   for (let i = 0; i < retries; i++) {
     try {
-      await checkHealth(timeoutSeconds)
+      await checkHealth(db, timeoutSeconds)
       console.log("Database connection successful.")
       return
     } catch (error) {
