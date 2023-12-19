8a3c31b0780f38009352f9308c63179f7eb65ec1
Marlen Brunner <klondikemarlen@gmail.com>
2023-12-19 16:33:30 -0700

    :beetle: Fix formatting lost on second render of component.
    e.g. toggling between tabs on the Employees tab of the child care centre details view.
    http://localhost:8080/child-care-centres/1/2023-24/employees/april to http://localhost:8080/child-care-centres/1/2023-24/employees/may and then back resulted in 5000 instead of $5,000.00
    
    This also, incidentally fixes the jump to end cursor bug. I'm not really sure how.
    Possibly because I'm no longer passing the value to the v-text field? And am instead simply relying on the library to do that for me? I guess the library just knows to load/inject this.modelValue?

--- web/src/components/CurrencyInput.vue
+++ web/src/components/CurrencyInput.vue
@@ -1,39 +1,30 @@
 <template>
-  <!--
-    Supress default update:model-value emit, then triggers on blur instead
-    This fixes the annoying cursor jump to end bug
-  -->
   <v-text-field
     ref="inputRef"
-    :model-value="modelValue"
     type="text"
     @blur="onBlur"
-    @update:model-value="false"
   />
 </template>
 
 <script setup lang="ts">
-import { useCurrencyInput, type CurrencyInputOptions } from "vue-currency-input"
+import { useCurrencyInput, type CurrencyInputOptions, CurrencyDisplay } from "vue-currency-input"
 
-const props = defineProps({
-  modelValue: Number,
-  options: {
-    type: Object,
-    default: () => ({
-      currency: "CAD",
-      locale: "en-CA",
-      currencyDisplay: "symbol",
-      precision: 2,
-      // accountingSign: true,
-      hideCurrencySymbolOnFocus: false,
-    }),
-  },
+const props = defineProps<{
+  modelValue: number | null
+  options?: Partial<CurrencyInputOptions>
+}>()
+
+const emit = defineEmits(["update:modelValue", "change"])
+
+const { inputRef, numberValue } = useCurrencyInput({
+  currency: "CAD",
+  locale: "en-CA",
+  currencyDisplay: CurrencyDisplay.symbol,
+  precision: 2,
+  // accountingSign: true,
+  hideCurrencySymbolOnFocus: false,
+  ...props.options,
 })
-
-const emit = defineEmits(["update:modelValue"])
-
-const autoEmit = false
-const { inputRef, numberValue } = useCurrencyInput(props.options as CurrencyInputOptions, autoEmit)
 
 function onBlur() {
   emit("update:modelValue", numberValue.value)
