a5ab7198e84e19e4ac41a0c9a41e0b305a7d0bc1
Marlen Brunner <klondikemarlen@gmail.com>
2024-03-08 16:32:42 -0700

    :recycle: Migrate CentreEditor to newer pattern.
    Note the editor is also the creation dialog.

--- web/src/modules/centre/components/CentreEditor.vue
+++ web/src/modules/centre/components/CentreEditor.vue
@@ -4,98 +4,114 @@
     persistent
     max-width="500"
   >
-    <v-card v-if="editingCentre">
-      <v-toolbar
-        color="primary"
-        variant="dark"
-        title="Centre Edit"
-      >
-        <v-spacer></v-spacer>
-        <v-btn
-          icon
-          color="white"
-          @click="close"
-          ><v-icon>mdi-close</v-icon></v-btn
-        >
-      </v-toolbar>
-      <v-card-text>
-        <v-text-field
-          v-model="editingCentre.name"
-          label="Name"
-          variant="outlined"
-          density="comfortable"
-        ></v-text-field>
-        <v-text-field
-          v-model="editingCentre.license"
-          label="License"
-          variant="outlined"
-          density="comfortable"
-        ></v-text-field>
-        <v-text-field
-          v-model="editingCentre.licensedFor"
-          label="Licensed for"
-          variant="outlined"
-          density="comfortable"
-        ></v-text-field>
-        <v-text-field
-          v-model="editingCentre.community"
-          label="Community"
-          variant="outlined"
-          density="comfortable"
-        ></v-text-field>
-        <v-checkbox
-          v-model="editingCentre.hotMeal"
-          label="Hot meal?"
-          variant="outlined"
-          density="comfortable"
-        ></v-checkbox>
-      </v-card-text>
-      <v-card-actions class="mx-4 mb-2">
-        <v-btn
+    <v-skeleton-loader
+      v-if="isNil(editingCentre)"
+      type="card"
+    />
+    <v-form
+      v-else
+      v-model="isValid"
+    >
+      <v-card>
+        <v-toolbar
           color="primary"
-          variant="flat"
-          :disabled="!canSave"
-          @click="saveClick"
-          >Save</v-btn
+          variant="dark"
+          title="Centre Edit"
         >
-        <v-spacer></v-spacer>
-        <v-btn
-          color="yg_sun"
-          variant="outlined"
-          @click="close"
-          >Close</v-btn
-        >
-      </v-card-actions>
-    </v-card>
+          <v-spacer></v-spacer>
+          <v-btn
+            icon
+            color="white"
+            @click="close"
+            ><v-icon>mdi-close</v-icon></v-btn
+          >
+        </v-toolbar>
+        <v-card-text>
+          <v-text-field
+            v-model="editingCentre.name"
+            label="Name *"
+            :rules="[required]"
+            variant="outlined"
+            density="comfortable"
+            required
+          ></v-text-field>
+          <v-text-field
+            v-model="editingCentre.license"
+            label="License"
+            variant="outlined"
+            density="comfortable"
+          ></v-text-field>
+          <v-text-field
+            v-model="editingCentre.licensedFor"
+            label="Licensed for"
+            variant="outlined"
+            density="comfortable"
+          ></v-text-field>
+          <v-text-field
+            v-model="editingCentre.community"
+            label="Community *"
+            :rules="[required]"
+            variant="outlined"
+            density="comfortable"
+            required
+          ></v-text-field>
+          <v-checkbox
+            v-model="editingCentre.hotMeal"
+            label="Hot meal?"
+            variant="outlined"
+            density="comfortable"
+          ></v-checkbox>
+        </v-card-text>
+        <v-card-actions class="mx-4 mb-2">
+          <v-btn
+            color="yg_sun"
+            variant="outlined"
+            @click="close"
+            >Close</v-btn
+          >
+          <v-spacer></v-spacer>
+          <v-btn
+            color="primary"
+            variant="flat"
+            :disabled="!isValid"
+            @click="save"
+            >Save</v-btn
+          >
+        </v-card-actions>
+      </v-card>
+    </v-form>
   </v-dialog>
 </template>
 
-<script lang="ts">
-import { mapActions, mapState, mapWritableState } from "pinia"
-import { useCentreStore } from "../store"
+<script setup lang="ts">
+import { computed, ref } from "vue"
+import { useRouter } from "vue-router"
+import { isNil } from "lodash"
 
-export default {
-  name: "CentreEditor",
-  data: () => ({}),
-  computed: {
-    ...mapState(useCentreStore, ["selectedCentre"]),
-    ...mapWritableState(useCentreStore, ["editingCentre"]),
-    visible() {
-      return !!this.editingCentre
-    },
-    canSave() {
-      return this.editingCentre?.name && this.editingCentre?.community
-    },
-  },
-  methods: {
-    ...mapActions(useCentreStore, ["save", "doneEdit"]),
-    close() {
-      this.doneEdit()
-    },
-    async saveClick() {
-      await this.save()
-      if (this.selectedCentre) this.$router.push(`/child-care-centres/${this.selectedCentre.id}`)
-    },
-  },
+import { required } from "@/utils/validators"
+import { useCentreStore } from "@/modules/centre/store"
+
+const router = useRouter()
+const centreStore = useCentreStore()
+
+const selectedCentre = computed(() => centreStore.selectedCentre)
+const editingCentre = computed(() => centreStore.editingCentre)
+const visible = computed(() => !!editingCentre.value)
+
+const isValid = ref(false)
+
+function close() {
+  centreStore.doneEdit()
+}
+
+async function save() {
+  await centreStore.save()
+
+  if (selectedCentre.value) {
+    router.push({
+      name: "CentreDashboardPage",
+      params: { centreId: selectedCentre.value.id },
+    })
+  }
 }
 </script>
