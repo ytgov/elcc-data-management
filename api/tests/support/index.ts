6321dd8bfe6f872ba9d7e289c61ddcd6225fdb30
Marlen Brunner <klondikemarlen@gmail.com>
2024-06-03 15:02:23 -0700

    :recycle: Add mock current user support helper.
    This will be useful for testing future endpoints.

--- api/tests/support/index.ts
+++ api/tests/support/index.ts
@@ -1,1 +1,2 @@
+export { mockCurrentUser } from "./mock-current-user"
 export { request } from "./request"
---
+++ api/tests/support/mock-current-user.ts
@@ -0,0 +1,30 @@
+import { Request, Response, NextFunction } from "express"
+
+import { checkJwt, autheticateAndLoadUser } from "@/middleware/authz.middleware"
+
+import { User } from "@/models"
+
+/**
+ * Usage:
+ * At the top level of a test file import:
+ *   import { mockCurrentUser } from "@/support"
+ *
+ * Then where you want to set the current user:
+ *   mockCurrentUser(currentUser)
+ *
+ * @param newCurrentUser - The user to set as the current user
+ */
+export function mockCurrentUser(newCurrentUser: User) {
+  vi.mock("@/middleware/authz.middleware")
+
+  const mockedCheckJwt = vi.mocked(checkJwt)
+  mockedCheckJwt.mockImplementation((req: Request, _res: Response, next: NextFunction) => next())
+
+  const mockedAutheticateAndLoadUser = vi.mocked(autheticateAndLoadUser)
+  mockedAutheticateAndLoadUser.mockImplementation(
+    async (req: Request, _res: Response, next: NextFunction) => {
+      req.user = newCurrentUser
+      next()
+    }
+  )
+}
