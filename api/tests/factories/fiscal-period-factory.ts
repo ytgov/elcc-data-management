d993de244f5991b932d94a3f20221eaf50ad2e9e
Marlen Brunner <klondikemarlen@gmail.com>
2024-06-04 16:24:00 -0700

    :factory: Add a bunch more factories so I can test the wage enhancement replication service.

---
+++ api/tests/factories/fiscal-period-factory.ts
@@ -0,0 +1,30 @@
+import { Factory } from "fishery"
+import { faker } from "@faker-js/faker"
+import { DateTime } from "luxon"
+
+import { FiscalPeriod } from "@/models"
+import { formatAsFiscalYear } from "@/factories/helpers"
+
+export const fiscalPeriodFactory = Factory.define<FiscalPeriod>(({ sequence, onCreate }) => {
+  onCreate((fiscalPeriod) => fiscalPeriod.save())
+
+  const year = faker.number.int({ min: 2022, max: 2027 })
+  const month = faker.number.int({ min: 1, max: 12 })
+  const dateStart = DateTime.local(year, month, 1).startOf("month")
+  const dateEnd = dateStart.endOf("month")
+  const monthName = dateStart.toFormat("MMMM")
+
+  const APRIL = 4 // Luxon uses 1-indexed months
+  const fiscalYearStartYear = month >= APRIL ? year : year - 1
+  const fiscalYear = formatAsFiscalYear(fiscalYearStartYear, { separator: "-" })
+
+  return FiscalPeriod.build({
+    id: sequence,
+    fiscalYear,
+    month: monthName,
+    dateStart: dateStart.toJSDate(),
+    dateEnd: dateEnd.toJSDate(),
+  })
+})
+
+export default fiscalPeriodFactory
