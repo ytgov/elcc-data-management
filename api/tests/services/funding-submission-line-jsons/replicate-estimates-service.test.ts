91aa7eb8f5af82826d404bee857f7bf06efaf895
Marlen Brunner <klondikemarlen@gmail.com>
2024-06-04 09:26:20 -0700

    :white_check_mark: Add initial test for replicating estimates.

--- api/tests/services/funding-submission-line-jsons/replicate-estimates-service.test.ts
+++ api/tests/services/funding-submission-line-jsons/replicate-estimates-service.test.ts
@@ -1,20 +1,56 @@
-import { fundingLineValueFactory, fundingSubmissionLineJsonFactory } from "@/factories"
 import { ReplicateEstimatesService } from "@/services/funding-submission-line-jsons"
+import {
+  centreFactory,
+  fundingLineValueFactory,
+  fundingSubmissionLineFactory,
+  fundingSubmissionLineJsonFactory,
+} from "@/factories"
 
 describe("api/src/services/funding-submission-line-jsons/replicate-estimates-service.ts", () => {
   describe("ReplicateEstimatesService", () => {
     describe("#perform", () => {
       test("when provided with a fundingSubmissionLineJson, it replicates the estimates to future submissions", async () => {
         // Arrange
-        const fundingLineValue1 = fundingLineValueFactory.build()
+        const centre1 = await centreFactory.create()
+        const submissionLine1 = await fundingSubmissionLineFactory.create()
+        const submissionLine2 = await fundingSubmissionLineFactory.create()
+        const fundingLineValue1 = fundingLineValueFactory.build({
+          submissionLineId: submissionLine1.id,
+        })
+        const fundingLineValue2 = fundingLineValueFactory.build({
+          submissionLineId: submissionLine2.id,
+        })
         const fundingSubmissionLineJson1 = await fundingSubmissionLineJsonFactory.create({
+          centreId: centre1.id,
+          fiscalYear: "2023/24",
+          dateStart: new Date("2023-04-01T00:00:00Z"),
+          dateEnd: new Date("2023-04-30T23:59:59Z"),
+          dateName: "April",
           lines: [fundingLineValue1],
         })
+        const fundingSubmissionLineJson2 = await fundingSubmissionLineJsonFactory.create({
+          centreId: centre1.id,
+          fiscalYear: "2023/24",
+          dateStart: new Date("2023-05-01T00:00:00Z"),
+          dateEnd: new Date("2023-05-31T23:59:59Z"),
+          dateName: "May",
+          lines: [fundingLineValue2],
+        })
+
         // Act
         await ReplicateEstimatesService.perform(fundingSubmissionLineJson1)
 
         // Assert
-        console.log(`fundingSubmissionLineJson1:`, fundingSubmissionLineJson1)
+        await fundingSubmissionLineJson2.reload()
+        expect(fundingSubmissionLineJson2.lines).toEqual(
+          expect.arrayContaining([
+            expect.objectContaining({
+              sectionName: fundingLineValue1.sectionName,
+              lineName: fundingLineValue1.lineName,
+              monthlyAmount: fundingLineValue1.monthlyAmount,
+            }),
+          ])
+        )
       })
     })
   })
