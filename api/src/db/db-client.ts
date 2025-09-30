d03ad012110c798aa7f1dd0acf80085e18235360
Marlen Brunner <klondikemarlen@gmail.com>
2025-09-30 16:41:35 -0700

    :construction: Rework db-client to use @sequelize/core and @sequelize/mssql.

--- api/src/db/db-client.ts
+++ api/src/db/db-client.ts
@@ -1,34 +1,50 @@
-import { Sequelize, Options } from "sequelize"
-import { createNamespace } from "cls-hooked"
+import { Sequelize, Options } from "@sequelize/core"
+import { MsSqlDialect } from "@sequelize/mssql"
+import { isEmpty, isNil } from "lodash"
 
-import { DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT, NODE_ENV } from "@/config"
+import {
+  DB_HOST,
+  DB_NAME,
+  DB_PASS,
+  DB_PORT,
+  DB_USER,
+  DB_TRUST_SERVER_CERTIFICATE,
+  NODE_ENV,
+} from "@/config"
 import compactSql from "@/db/utils/compact-sql"
 
-const namespace = createNamespace("sequelize-transaction-context")
-Sequelize.useCLS(namespace)
-
-if (DB_NAME === undefined) throw new Error("database name is unset.")
-if (DB_USER === undefined) throw new Error("database username is unset.")
-if (DB_PASS === undefined) throw new Error("database password is unset.")
-if (DB_HOST === undefined) throw new Error("database host is unset.")
-if (DB_PORT === undefined) throw new Error("database port is unset.")
+if (isEmpty(DB_NAME)) throw new Error("database name is unset.")
+if (isEmpty(DB_USER)) throw new Error("database username is unset.")
+if (isEmpty(DB_PASS)) throw new Error("database password is unset.")
+if (isEmpty(DB_HOST)) throw new Error("database host is unset.")
+if (isNil(DB_PORT)) throw new Error("database port is unset.")
 
 function sqlLogger(query: string) {
   console.log(compactSql(query))
 }
 
-export const SEQUELIZE_CONFIG: Options = {
-  username: DB_USER,
-  password: DB_PASS,
-  database: DB_NAME,
-  dialect: "mssql",
-  host: DB_HOST,
+// See https://sequelize.org/docs/v7/databases/mssql/
+export const SEQUELIZE_CONFIG: Options<MsSqlDialect> = {
+  dialect: MsSqlDialect,
+  server: DB_HOST,
   port: DB_PORT,
-  schema: "dbo",
+  database: DB_NAME,
+  encrypt: true,
+  authentication: {
+    type: "default",
+    options: {
+      userName: DB_USER,
+      password: DB_PASS,
+    },
+  },
+  schema: "dbo", // default - explicit for clarity
+  // Avoids need to have a signed certificate in development.
+  trustServerCertificate: DB_TRUST_SERVER_CERTIFICATE,
   logging: NODE_ENV === "development" ? sqlLogger : false,
   define: {
     underscored: true,
-    timestamps: true, // This is actually the default, but making it explicit for clarity.
+    timestamps: true, // default - explicit for clarity.
+    paranoid: true, // adds deleted_at column
   },
 }
 
