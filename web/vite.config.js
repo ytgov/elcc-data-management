ae6ba1a4a99f6cc5fedadb31a0387900ab867223
Marlen Brunner <klondikemarlen@gmail.com>
2025-09-30 10:50:07 -0700

    :gear: Update vite and tsconfig config to newer pattern used in other projects.

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,11 +1,12 @@
 /// <reference types="vitest" />
+import { fileURLToPath, URL } from "node:url"
+
 // Plugins
 import vue from "@vitejs/plugin-vue"
 import vuetify from "vite-plugin-vuetify"
 
 // Utilities
 import { defineConfig } from "vite"
-import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -22,8 +23,8 @@ export default defineConfig({
   define: { "process.env": {} },
   resolve: {
     alias: {
-      "@/": `${path.resolve(__dirname, "src")}/`,
-      "@models": `${path.resolve(__dirname, "../api/src/data/models")}`,
+      "@/tests": fileURLToPath(new URL("./tests", import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
     extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue"],
   },
