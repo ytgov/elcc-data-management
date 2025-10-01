2c4b1d953631a049521c68b588872a7049b9c072
Marlen Brunner <klondikemarlen@gmail.com>
2025-10-01 16:57:16 -0700

    :gear: Add ability for ESLint to ignore variables prefixed with an underscore.

--- web/.eslintrc.cjs
+++ web/.eslintrc.cjs
@@ -31,5 +31,17 @@ module.exports = {
         allowModifiers: true,
       },
     ],
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        args: "all",
+        argsIgnorePattern: "^_",
+        caughtErrors: "all",
+        caughtErrorsIgnorePattern: "^_",
+        destructuredArrayIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        ignoreRestSiblings: true,
+      },
+    ],
   },
 }
