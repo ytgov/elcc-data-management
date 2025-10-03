b609a08c210f9973ca5121fdd38ba429f2a1b40a
Marlen Brunner <klondikemarlen@gmail.com>
2025-10-03 15:11:52 -0700

    :art: Clean up DefaultLayout.vue and standarize to script setup.

--- web/src/layouts/DefaultLayout.vue
+++ web/src/layouts/DefaultLayout.vue
@@ -19,85 +19,80 @@
     >
 
     <template #append>
-      <div v-if="isAuthenticated">
-        <v-btn
-          color="primary"
-          class="mr-1"
-          to="/dashboard"
-          icon="mdi-home"
-        ></v-btn>
+      <v-btn
+        color="primary"
+        class="mr-1"
+        to="/dashboard"
+        icon="mdi-home"
+      ></v-btn>
 
-        <v-divider
-          class="mr-5"
-          vertical
-          inset
-        ></v-divider>
-        <span style="font-size: 0.9rem"> {{ username }} </span>
-        <span
-          class="pl-3"
-          @click="toggleAdmin()"
+      <v-divider
+        class="mr-5"
+        vertical
+        inset
+      ></v-divider>
+      <span style="font-size: 0.9rem"> {{ username }} </span>
+      <span
+        class="pl-3"
+        @click="toggleAdmin"
+      >
+        <v-chip
+          v-if="isAdmin"
+          color="yg_moss"
         >
-          <v-chip
+          Admin
+        </v-chip>
+        <v-chip
+          v-else
+          color="yg_twilight"
+        >
+          User
+        </v-chip>
+      </span>
+
+      <v-menu offset-y>
+        <template #activator="{ props }">
+          <v-btn
+            icon="mdi-dots-vertical"
+            color="primary"
+            v-bind="props"
+          ></v-btn>
+        </template>
+
+        <v-list density="compact">
+          <v-list-item to="/profile">
+            <template #prepend>
+              <v-icon>mdi-account</v-icon>
+            </template>
+            <v-list-item-title style="font-size: 0.9rem !important">My profile</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item
             v-if="isAdmin"
-            color="yg_moss"
+            to="/administration"
           >
-            Admin
-          </v-chip>
-          <v-chip
-            v-else
-            color="yg_twilight"
-          >
-            User
-          </v-chip>
-        </span>
-
-        <v-menu offset-y>
-          <template #activator="{ props }">
-            <v-btn
-              icon="mdi-dots-vertical"
-              color="primary"
-              v-bind="props"
-            ></v-btn>
-          </template>
-
-          <v-list density="compact">
-            <v-list-item to="/profile">
-              <template #prepend>
-                <v-icon>mdi-account</v-icon>
-              </template>
-              <v-list-item-title style="font-size: 0.9rem !important">My profile</v-list-item-title>
-            </v-list-item>
-
-            <v-list-item
-              v-if="isAdmin"
-              to="/administration"
+            <template #prepend>
+              <v-icon>mdi-cogs</v-icon>
+            </template>
+            <v-list-item-title style="font-size: 0.9rem !important"
+              >Administration</v-list-item-title
             >
-              <template #prepend>
-                <v-icon>mdi-cogs</v-icon>
-              </template>
-              <v-list-item-title style="font-size: 0.9rem !important"
-                >Administration</v-list-item-title
-              >
-            </v-list-item>
-            <v-divider />
-            <v-list-item :to="{ name: 'StatusPage' }">
-              <template #prepend>
-                <v-icon>mdi-clock</v-icon>
-              </template>
-              <v-list-item-title>{{ releaseTag }}</v-list-item-title>
-            </v-list-item>
-            <v-list-item @click="signOut">
-              <template #prepend>
-                <v-icon>mdi-exit-run</v-icon>
-              </template>
-              <v-list-item-title style="font-size: 0.9rem !important">Sign out</v-list-item-title>
-            </v-list-item>
-          </v-list>
-        </v-menu>
-      </div>
-      <div v-else>
-        <LoginButton />
-      </div>
+          </v-list-item>
+          <v-divider />
+          <v-list-item :to="{ name: 'StatusPage' }">
+            <template #prepend>
+              <v-icon>mdi-clock</v-icon>
+            </template>
+            <v-list-item-title>{{ releaseTag }}</v-list-item-title>
+          </v-list-item>
+          <v-list-item @click="signOut">
+            <template #prepend>
+              <v-icon>mdi-exit-run</v-icon>
+            </template>
+            <v-list-item-title style="font-size: 0.9rem !important">Sign out</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </template>
   </v-app-bar>
 
@@ -108,115 +103,78 @@
       fluid
       class="page-wrapper"
     >
+      <PageLoader v-if="isLoading" />
       <router-view></router-view>
     </v-container>
   </v-main>
-
-  <v-overlay
-    v-model="showOverlay"
-    class="align-center justify-center"
-  >
-    <div class="text-center">
-      <v-progress-circular
-        indeterminate
-        size="64"
-        class="mb-5"
-        color="#f3b228"
-        width="6"
-      ></v-progress-circular>
-      <h2>Loading {{ title }}</h2>
-    </div>
-  </v-overlay>
 </template>
 
-<script lang="ts">
-import { mapState, mapActions, mapWritableState } from "pinia"
+<script setup lang="ts">
+import { computed, onMounted, ref } from "vue"
 import { useAuth0 } from "@auth0/auth0-vue"
 
 import http from "@/api/http-client"
 
 import useCurrentUser from "@/use/use-current-user"
-import { useUserStore } from "@/store/UserStore"
-import { useNotificationStore } from "@/store/NotificationStore"
 
+import { useUserStore } from "@/store/UserStore"
 import { useCentreStore } from "@/modules/centre/store"
 import { useSubmissionLinesStore } from "@/modules/submission-lines/store"
 
-import LoginButton from "@/components/LoginButton.vue"
+import PageLoader from "@/components/common/PageLoader.vue"
 
 const UNSET_RELEASE_TAG = "2024.01.10.0"
 
-export default {
-  name: "DefaultLayout",
-  components: {
-    LoginButton,
-  },
-  setup() {
-    const { logout } = useAuth0()
-    const { currentUser, reset: resetCurrentUser } = useCurrentUser<true>()
+const { logout } = useAuth0()
+const { currentUser, isAdmin, reset: resetCurrentUser } = useCurrentUser<true>()
 
-    return {
-      logout,
-      currentUser,
-      resetCurrentUser,
-    }
-  },
-  data() {
-    return {
-      isAuthenticated: this.$auth0.isAuthenticated,
-      authUser: this.$auth0.user,
-      showOverlay: true,
-      releaseTag: UNSET_RELEASE_TAG,
-    }
-  },
-  computed: {
-    ...mapWritableState(useNotificationStore, ["showNotification"]),
-    ...mapState(useUserStore, ["user", "isAdmin"]),
+const releaseTag = ref(UNSET_RELEASE_TAG)
 
-    title() {
-      return "ELCC Data Management"
+const title = computed(() => "ELCC Data Management")
+const username = computed(() => currentUser.value?.displayName)
+
+const userStore = useUserStore()
+const centreStore = useCentreStore()
+const submissionLinesStore = useSubmissionLinesStore()
+
+const isLoading = ref(true)
+
+onMounted(async () => {
+  await Promise.all([
+    userStore.initialize(),
+    centreStore.initialize(),
+    submissionLinesStore.initialize(),
+    fetchVersion(),
+  ])
+
+  isLoading.value = false
+})
+
+async function toggleAdmin() {
+  userStore.toggleAdmin()
+}
+
+function signOut() {
+  resetCurrentUser()
+
+  // TODO: add support for redirect to /sign-in
+  // const returnTo = encodeURI(window.location.origin + "/sign-in")
+  const returnTo = window.location.origin
+
+  return logout({
+    logoutParams: {
+      returnTo,
     },
-    username() {
-      return this.authUser?.name
-    },
-  },
+  })
+}
 
-  async mounted() {
-    await this.initialize()
-    await this.initCentres()
-    await this.initLines()
-    await this.fetchVersion()
-    this.showOverlay = false
-  },
-  methods: {
-    ...mapActions(useUserStore, ["initialize", "toggleAdmin"]),
-    ...mapActions(useCentreStore, { initCentres: "initialize" }),
-    ...mapActions(useSubmissionLinesStore, { initLines: "initialize" }),
-
-    signOut() {
-      this.resetCurrentUser()
-
-      // TODO: add support for redirect to /sign-in
-      // const returnTo = encodeURI(window.location.origin + "/sign-in")
-      const returnTo = window.location.origin
-
-      return this.logout({
-        logoutParams: {
-          returnTo,
-        },
-      })
-    },
-    fetchVersion() {
-      http
-        .get("/_status")
-        .then(({ data }) => {
-          this.releaseTag = data.RELEASE_TAG
-        })
-        .catch((error: unknown) => {
-          console.error(`Error fetching version: ${error}`)
-        })
-    },
-  },
+async function fetchVersion() {
+  try {
+    const { data } = await http.get("/_status")
+    releaseTag.value = data.RELEASE_TAG
+  } catch (error: unknown) {
+    console.error(`Error fetching version: ${error}`, { error })
+  }
 }
 </script>
 
