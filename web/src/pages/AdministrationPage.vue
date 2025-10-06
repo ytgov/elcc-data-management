695c1060f211ecd6226b0cc3fdad07ecd0f96cbe
Marlen Brunner <klondikemarlen@gmail.com>
2025-10-06 13:34:23 -0700

    :sparkles: Implement model counts for admin dashboard.

--- web/src/pages/AdministrationPage.vue
+++ web/src/pages/AdministrationPage.vue
@@ -28,9 +28,7 @@
       <v-card to="/administration/users">
         <v-card-title>Users</v-card-title>
         <v-card-text>
-          <div style="font-size: 30px">
-            {{ usersTotalCount }}
-          </div>
+          <span class="text-h4">{{ usersTotalCount }}</span>
         </v-card-text>
       </v-card>
     </v-col>
@@ -38,15 +36,15 @@
       <v-card to="/administration/funding-periods">
         <v-card-title>Funding Periods</v-card-title>
         <v-card-text>
-          <div style="font-size: 30px">23</div>
+          <span class="text-h4">{{ fundingPeriodsTotalCount }}</span>
         </v-card-text>
       </v-card>
     </v-col>
     <v-col>
       <v-card to="/administration/submission-lines">
-        <v-card-title>Submission Format</v-card-title>
+        <v-card-title>Submission Lines</v-card-title>
         <v-card-text>
-          <div style="font-size: 30px">23</div>
+          <span class="text-h4">{{ fundingSubmissionLinesTotalCount }}</span>
         </v-card-text>
       </v-card>
     </v-col>
@@ -56,12 +54,26 @@
 <script setup lang="ts">
 import { computed } from "vue"
 
+import useFundingPeriods from "@/use/use-funding-periods"
+import useFundingSubmissionLines from "@/use/use-funding-submission-lines"
 import useUsers from "@/use/use-users"
 
 const usersQuery = computed(() => ({
   perPage: 1,
 }))
 const { totalCount: usersTotalCount } = useUsers(usersQuery)
+
+const fundingPeriodsQuery = computed(() => ({
+  perPage: 1,
+}))
+const { totalCount: fundingPeriodsTotalCount } = useFundingPeriods(fundingPeriodsQuery)
+
+const fundingSubmissionLinesQuery = computed(() => ({
+  perPage: 1,
+}))
+const { totalCount: fundingSubmissionLinesTotalCount } = useFundingSubmissionLines(
+  fundingSubmissionLinesQuery
+)
 
 const breadcrumbs = [
   {
