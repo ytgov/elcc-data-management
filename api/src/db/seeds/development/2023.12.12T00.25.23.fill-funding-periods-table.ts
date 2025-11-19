e4a1f91338599568da809325af327a6c78b8bc33
Marlen Brunner <klondikemarlen@gmail.com>
2025-11-19 15:51:15 -0700

    :sparkles: Add funding periods seed files using CreateService
    Seed files create 5 fiscal year funding periods (2022-2027), which automatically create fiscal periods and employee wage tiers via the service.

---
+++ api/src/db/seeds/development/2023.12.12T00.25.23.fill-funding-periods-table.ts
@@ -0,0 +1,64 @@
+import { CreationAttributes } from "@sequelize/core"
+import { isNil } from "lodash"
+
+import type { SeedMigration } from "@/db/umzug"
+import { FundingPeriod } from "@/models"
+import { CreateService } from "@/services/funding-periods"
+
+export const up: SeedMigration = async () => {
+  const fundingPeriodsAttributes: CreationAttributes<FundingPeriod>[] = [
+    {
+      fiscalYear: "2022-2023",
+      fromDate: new Date("2022-04-01"),
+      toDate: new Date("2023-03-31"),
+      title: "Funding Period 2022-2023",
+      isFiscalYear: true,
+    },
+    {
+      fiscalYear: "2023-2024",
+      fromDate: new Date("2023-04-01"),
+      toDate: new Date("2024-03-31"),
+      title: "Funding Period 2023-2024",
+      isFiscalYear: true,
+    },
+    {
+      fiscalYear: "2024-2025",
+      fromDate: new Date("2024-04-01"),
+      toDate: new Date("2025-03-31"),
+      title: "Funding Period 2024-2025",
+      isFiscalYear: true,
+    },
+    {
+      fiscalYear: "2025-2026",
+      fromDate: new Date("2025-04-01"),
+      toDate: new Date("2026-03-31"),
+      title: "Funding Period 2025-2026",
+      isFiscalYear: true,
+    },
+    {
+      fiscalYear: "2026-2027",
+      fromDate: new Date("2026-04-01"),
+      toDate: new Date("2027-03-31"),
+      title: "Funding Period 2026-2027",
+      isFiscalYear: true,
+    },
+  ]
+
+  for (const fundingPeriodAttributes of fundingPeriodsAttributes) {
+    const fundingPeriod = await FundingPeriod.findOne({
+      where: {
+        fiscalYear: fundingPeriodAttributes.fiscalYear,
+        isFiscalYear: true,
+      },
+    })
+
+    if (isNil(fundingPeriod)) {
+      await CreateService.perform(fundingPeriodAttributes)
+    }
+  }
+}
+
+export const down: SeedMigration = async () => {
+  // this method needs to exist, but does not need to be implemented.
+  // Seeds should be idempotent.
+}
