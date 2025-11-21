69575debb455696a84760db86b62e22d0a61b65f
Marlen Brunner <klondikemarlen@gmail.com>
2025-11-21 10:40:29 -0700

    :recycle: Standardize breadcrumbs at default layout level.
    Why? This makes it easier to add breadcrumbs to pages and make the overall app layout more consistent.
    It's a bit uglier currently, but that can be resolved over time.

--- web/src/layouts/DefaultLayout.vue
+++ web/src/layouts/DefaultLayout.vue
@@ -9,6 +9,16 @@
       class="page-wrapper"
     >
       <PageLoader v-if="isLoading" />
+
+      <ExactingBreadcrumbs
+        class="pl-4 mb-4 mb-2 mt-n3 mx-n4 text-body-1 font-weight-bold"
+        bg-color="yg-moss"
+        color="white"
+        active-color="#fff"
+        :items="breadcrumbs"
+      />
+      <h2 class="text-h4 mb-4">{{ title }}</h2>
+
       <router-view></router-view>
     </v-container>
   </v-main>
@@ -20,9 +30,11 @@ import { onMounted, ref } from "vue"
 import { useUserStore } from "@/store/UserStore"
 import { useCentreStore } from "@/modules/centre/store"
 import { useSubmissionLinesStore } from "@/modules/submission-lines/store"
+import useBreadcrumbs from "@/use/use-breadcrumbs"
 
 import PageLoader from "@/components/common/PageLoader.vue"
 import DefaultAppBar from "@/components/layouts/DefaultAppBar.vue"
+import ExactingBreadcrumbs from "@/components/layouts/ExactingBreadcrumbs.vue"
 
 const userStore = useUserStore()
 const centreStore = useCentreStore()
@@ -39,4 +51,13 @@ onMounted(async () => {
 
   isLoading.value = false
 })
+
+const { title, breadcrumbs } = useBreadcrumbs()
 </script>
+
+<style scoped>
+/* Breadcrumb disabled state styling */
+:deep(.v-breadcrumbs-item--disabled) {
+  opacity: 0.7 !important;
+}
+</style>
--- web/src/routes/administration-routes.ts
+++ web/src/routes/administration-routes.ts
@@ -4,49 +4,43 @@ const routes = [
     component: () => import("@/layouts/DefaultLayout.vue"),
     children: [
       {
-        path: "",
-        component: () => import("@/layouts/LayoutWithBreadcrumbs.vue"),
-        children: [
-          {
-            path: "administration",
-            name: "AdministrationPage",
-            component: () => import("@/pages/AdministrationPage.vue"),
-          },
-          {
-            path: "administration/users",
-            name: "administration/UsersPage",
-            component: () => import("@/pages/administration/AdminstrationUsersPage.vue"),
-          },
-          {
-            path: "administration/users/:userId",
-            name: "administration/users/UserPage",
-            component: () => import("@/pages/administration/users/UserPage.vue"),
-            props: true,
-          },
-          {
-            path: "administration/funding-periods",
-            name: "administration/FundingPeriodsPage",
-            component: () => import("@/pages/administration/AdministrationFundingPeriodsPage.vue"),
-          },
-          {
-            path: "administration/funding-periods/:fundingPeriodId",
-            name: "administration/funding-periods/FundingPeriodPage",
-            component: () => import("@/pages/administration/funding-periods/FundingPeriodPage.vue"),
-            props: true
-          },
-          {
-            path: "administration/submission-lines",
-            name: "administration/AdministrationSubmissionLinesPage",
-            component: () => import("@/pages/administration/AdministrationSubmissionLinesPage.vue"),
-          },
-          {
-            path: "administration/submission-lines/:fundingSubmissionLineId",
-            name: "administration/submission-lines/SubmissionLinePage",
-            component: () =>
-              import("@/pages/administration/submission-lines/SubmissionLinePage.vue"),
-            props: true,
-          },
-        ],
+        path: "administration",
+        name: "AdministrationPage",
+        component: () => import("@/pages/AdministrationPage.vue"),
+      },
+      {
+        path: "administration/users",
+        name: "administration/UsersPage",
+        component: () => import("@/pages/administration/AdminstrationUsersPage.vue"),
+      },
+      {
+        path: "administration/users/:userId",
+        name: "administration/users/UserPage",
+        component: () => import("@/pages/administration/users/UserPage.vue"),
+        props: true,
+      },
+      {
+        path: "administration/funding-periods",
+        name: "administration/FundingPeriodsPage",
+        component: () => import("@/pages/administration/AdministrationFundingPeriodsPage.vue"),
+      },
+      {
+        path: "administration/funding-periods/:fundingPeriodId",
+        name: "administration/funding-periods/FundingPeriodPage",
+        component: () => import("@/pages/administration/funding-periods/FundingPeriodPage.vue"),
+        props: true
+      },
+      {
+        path: "administration/submission-lines",
+        name: "administration/AdministrationSubmissionLinesPage",
+        component: () => import("@/pages/administration/AdministrationSubmissionLinesPage.vue"),
+      },
+      {
+        path: "administration/submission-lines/:fundingSubmissionLineId",
+        name: "administration/submission-lines/SubmissionLinePage",
+        component: () =>
+          import("@/pages/administration/submission-lines/SubmissionLinePage.vue"),
+        props: true,
       },
     ],
   },
