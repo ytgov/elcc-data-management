0952f8bd231c69bdf19e590bad5868aa2f6d4106
Marlen Brunner <klondikemarlen@gmail.com>
2025-11-25 11:31:25 -0700

    :bug: Fix funding reconciliation adjustments seeds.
    Previous seeds where unlinked to the funding period association so were in a different year.

--- api/src/db/seeds/production/2025.11.24T12.39.17.fill-funding-reconciliation-adjustments-table.ts
+++ api/src/db/seeds/production/2025.11.24T12.39.17.fill-funding-reconciliation-adjustments-table.ts
@@ -1,29 +1,49 @@
-import { isNil } from "lodash"
+import { isNil, isUndefined } from "lodash"
 
 import { FiscalPeriod, FundingReconciliation, FundingReconciliationAdjustment } from "@/models"
 
 export async function up() {
-  await FundingReconciliation.findEach(async (fundingReconciliation) => {
-    await FiscalPeriod.findEach(async (fiscalPeriod) => {
-      const existingAdjustment = await FundingReconciliationAdjustment.findOne({
-        where: {
-          fundingReconciliationId: fundingReconciliation.id,
-          fiscalPeriodId: fiscalPeriod.id,
-        },
-      })
-
-      if (isNil(existingAdjustment)) {
-        await FundingReconciliationAdjustment.create({
-          fundingReconciliationId: fundingReconciliation.id,
-          fiscalPeriodId: fiscalPeriod.id,
-          fundingReceivedPeriodAmount: "0",
-          eligibleExpensesPeriodAmount: "0",
-          payrollAdjustmentsPeriodAmount: "0",
-          cumulativeBalanceAmount: "0",
-        })
+  await FundingReconciliation.findEach(
+    {
+      include: ["fundingPeriod"],
+    },
+    async (fundingReconciliation) => {
+      const { fundingPeriod } = fundingReconciliation
+      if (isUndefined(fundingPeriod)) {
+        throw new Error("Expected fundingPeriod association to be pre-loaded.")
       }
-    })
-  })
+
+      const { fiscalYear } = fundingPeriod
+      const fiscalYearShort = FiscalPeriod.toShortFiscalYearFormat(fiscalYear)
+
+      await FiscalPeriod.findEach(
+        {
+          where: {
+            fiscalYear: fiscalYearShort,
+          },
+        },
+        async (fiscalPeriod) => {
+          const existingAdjustment = await FundingReconciliationAdjustment.findOne({
+            where: {
+              fundingReconciliationId: fundingReconciliation.id,
+              fiscalPeriodId: fiscalPeriod.id,
+            },
+          })
+
+          if (isNil(existingAdjustment)) {
+            await FundingReconciliationAdjustment.create({
+              fundingReconciliationId: fundingReconciliation.id,
+              fiscalPeriodId: fiscalPeriod.id,
+              fundingReceivedPeriodAmount: "0",
+              eligibleExpensesPeriodAmount: "0",
+              payrollAdjustmentsPeriodAmount: "0",
+              cumulativeBalanceAmount: "0",
+            })
+          }
+        }
+      )
+    }
+  )
 }
 
 export async function down() {
