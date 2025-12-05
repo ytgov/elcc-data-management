8768ea2eb4d20e8cea0264ac04ec4bd05f6d34ee
Marlen Brunner <klondikemarlen@gmail.com>
2025-12-05 13:40:35 -0700

    :japanese_castle: Block funding region and centre deletion when dependent entities exist.

--- api/src/services/centres/destroy-service.ts
+++ api/src/services/centres/destroy-service.ts
@@ -1,4 +1,13 @@
-import { Centre, User } from "@/models"
+import {
+  BuildingExpense,
+  Centre,
+  EmployeeBenefit,
+  FundingReconciliation,
+  FundingSubmissionLineJson,
+  Payment,
+  User,
+  WageEnhancement,
+} from "@/models"
 import BaseService from "@/services/base-service"
 
 export class DestroyService extends BaseService {
@@ -10,7 +19,90 @@ export class DestroyService extends BaseService {
   }
 
   async perform(): Promise<void> {
+    await this.assertNoDependentEntitiesExist()
+
     await this.centre.destroy()
+  }
+
+  private async assertNoDependentEntitiesExist() {
+    await this.assertNoDependentBuildingExpensesExist()
+    await this.assertNoDependentEmployeeBenefitsExist()
+    await this.assertNoDependentFundingSubmissionLineJsonsExist()
+    await this.assertNoDependentPaymentsExist()
+    await this.assertNoDependentWageEnhancementsExist()
+    await this.assertNoDependentFundingReconciliationsExist()
+  }
+
+  private async assertNoDependentBuildingExpensesExist() {
+    const buildingExpenseCount = await BuildingExpense.count({
+      where: {
+        centreId: this.centre.id,
+      },
+    })
+
+    if (buildingExpenseCount > 0) {
+      throw new Error("Centre with building expenses cannot be deleted")
+    }
+  }
+
+  private async assertNoDependentEmployeeBenefitsExist() {
+    const employeeBenefitCount = await EmployeeBenefit.count({
+      where: {
+        centreId: this.centre.id,
+      },
+    })
+
+    if (employeeBenefitCount > 0) {
+      throw new Error("Centre with employee benefits cannot be deleted")
+    }
+  }
+
+  private async assertNoDependentFundingSubmissionLineJsonsExist() {
+    const fundingSubmissionLineJsonCount = await FundingSubmissionLineJson.count({
+      where: {
+        centreId: this.centre.id,
+      },
+    })
+
+    if (fundingSubmissionLineJsonCount > 0) {
+      throw new Error("Centre with funding submission line jsons cannot be deleted")
+    }
+  }
+
+  private async assertNoDependentPaymentsExist() {
+    const paymentCount = await Payment.count({
+      where: {
+        centreId: this.centre.id,
+      },
+    })
+
+    if (paymentCount > 0) {
+      throw new Error("Centre with payments cannot be deleted")
+    }
+  }
+
+  private async assertNoDependentWageEnhancementsExist() {
+    const wageEnhancementCount = await WageEnhancement.count({
+      where: {
+        centreId: this.centre.id,
+      },
+    })
+
+    if (wageEnhancementCount > 0) {
+      throw new Error("Centre with wage enhancements cannot be deleted")
+    }
+  }
+
+  private async assertNoDependentFundingReconciliationsExist() {
+    const fundingReconciliationCount = await FundingReconciliation.count({
+      where: {
+        centreId: this.centre.id,
+      },
+    })
+
+    if (fundingReconciliationCount > 0) {
+      throw new Error("Centre with funding reconciliations cannot be deleted")
+    }
   }
 }
 
--- api/src/services/funding-regions/destroy-service.ts
+++ api/src/services/funding-regions/destroy-service.ts
@@ -1,4 +1,4 @@
-import { FundingRegion, User } from "@/models"
+import { BuildingExpenseCategory, FundingRegion, User } from "@/models"
 import BaseService from "@/services/base-service"
 
 export class DestroyService extends BaseService {
@@ -10,7 +10,25 @@ export class DestroyService extends BaseService {
   }
 
   async perform(): Promise<void> {
+    await this.assertNoDependentEntitiesExist()
+
     await this.fundingRegion.destroy()
+  }
+
+  private async assertNoDependentEntitiesExist() {
+    await this.assertNoDependentBuildingExpenseCategoriesExist()
+  }
+
+  private async assertNoDependentBuildingExpenseCategoriesExist() {
+    const buildingExpenseCategoryCount = await BuildingExpenseCategory.count({
+      where: {
+        fundingRegionId: this.fundingRegion.id,
+      },
+    })
+
+    if (buildingExpenseCategoryCount > 0) {
+      throw new Error("Funding region with building expense categories cannot be deleted")
+    }
   }
 }
 
