dead7b05a40cc9601f9b47070eab9dc18b2e1318
Marlen Brunner <klondikemarlen@gmail.com>
2025-12-10 14:48:51 -0800

    :unlock: Permit centres to be destroyed, and destroy all dependent entities.
    Also add tests.

--- api/src/services/centres/destroy-service.ts
+++ api/src/services/centres/destroy-service.ts
@@ -22,7 +22,7 @@ export class DestroyService extends BaseService {
 
   async perform(): Promise<void> {
     await db.transaction(async () => {
-      await this.assertNoDependentEntitiesExist()
+      await this.destroyDependentEntities()
 
       await this.centre.destroy()
 
@@ -30,85 +30,65 @@ export class DestroyService extends BaseService {
     })
   }
 
-  private async assertNoDependentEntitiesExist() {
-    await this.assertNoDependentBuildingExpensesExist()
-    await this.assertNoDependentEmployeeBenefitsExist()
-    await this.assertNoDependentFundingSubmissionLineJsonsExist()
-    await this.assertNoDependentPaymentsExist()
-    await this.assertNoDependentWageEnhancementsExist()
-    await this.assertNoDependentFundingReconciliationsExist()
+  private async destroyDependentEntities() {
+    await this.destroyDependentBuildingExpenses()
+    await this.destroyDependentEmployeeBenefits()
+    await this.destroyDependentFundingSubmissionLineJsons()
+    await this.destroyDependentPayments()
+    await this.destroyDependentWageEnhancements()
+    await this.destroyDependentFundingReconciliations()
   }
 
-  private async assertNoDependentBuildingExpensesExist() {
-    const buildingExpenseCount = await BuildingExpense.count({
+  private async destroyDependentBuildingExpenses() {
+    await BuildingExpense.destroy({
+      where: {
+        centreId: this.centre.id,
+      },
+      // TODO: remove this once all dependent models support soft deletion.
+      force: true,
+    })
+  }
+
+  private async destroyDependentEmployeeBenefits() {
+    await EmployeeBenefit.destroy({
       where: {
         centreId: this.centre.id,
       },
     })
-
-    if (buildingExpenseCount > 0) {
-      throw new Error("Centre with building expenses cannot be deleted")
-    }
   }
 
-  private async assertNoDependentEmployeeBenefitsExist() {
-    const employeeBenefitCount = await EmployeeBenefit.count({
+  private async destroyDependentFundingSubmissionLineJsons() {
+    await FundingSubmissionLineJson.destroy({
       where: {
         centreId: this.centre.id,
       },
     })
-
-    if (employeeBenefitCount > 0) {
-      throw new Error("Centre with employee benefits cannot be deleted")
-    }
   }
 
-  private async assertNoDependentFundingSubmissionLineJsonsExist() {
-    const fundingSubmissionLineJsonCount = await FundingSubmissionLineJson.count({
+  private async destroyDependentPayments() {
+    await Payment.destroy({
       where: {
         centreId: this.centre.id,
       },
     })
-
-    if (fundingSubmissionLineJsonCount > 0) {
-      throw new Error("Centre with funding submission line jsons cannot be deleted")
-    }
   }
 
-  private async assertNoDependentPaymentsExist() {
-    const paymentCount = await Payment.count({
+  private async destroyDependentWageEnhancements() {
+    await WageEnhancement.destroy({
       where: {
         centreId: this.centre.id,
       },
     })
-
-    if (paymentCount > 0) {
-      throw new Error("Centre with payments cannot be deleted")
-    }
   }
 
-  private async assertNoDependentWageEnhancementsExist() {
-    const wageEnhancementCount = await WageEnhancement.count({
+  private async destroyDependentFundingReconciliations() {
+    await FundingReconciliation.destroy({
       where: {
         centreId: this.centre.id,
       },
+      // TODO: remove this once all dependent models support soft deletion.
+      force: true,
     })
-
-    if (wageEnhancementCount > 0) {
-      throw new Error("Centre with wage enhancements cannot be deleted")
-    }
-  }
-
-  private async assertNoDependentFundingReconciliationsExist() {
-    const fundingReconciliationCount = await FundingReconciliation.count({
-      where: {
-        centreId: this.centre.id,
-      },
-    })
-
-    if (fundingReconciliationCount > 0) {
-      throw new Error("Centre with funding reconciliations cannot be deleted")
-    }
   }
 
   private async logCentreDestruction(centre: Centre, currentUser: User) {
--- api/tests/services/centres/destroy-service.test.ts
+++ api/tests/services/centres/destroy-service.test.ts
@@ -1,6 +1,29 @@
-import { Centre, Log } from "@/models"
+import {
+  BuildingExpense,
+  Centre,
+  EmployeeBenefit,
+  FundingReconciliation,
+  FundingReconciliationAdjustment,
+  FundingSubmissionLineJson,
+  Log,
+  Payment,
+  WageEnhancement,
+} from "@/models"
 
-import { centreFactory, userFactory } from "@/factories"
+import {
+  buildingExpenseFactory,
+  centreFactory,
+  employeeBenefitFactory,
+  employeeWageTierFactory,
+  fiscalPeriodFactory,
+  fundingPeriodFactory,
+  fundingReconciliationAdjustmentFactory,
+  fundingReconciliationFactory,
+  fundingSubmissionLineJsonFactory,
+  paymentFactory,
+  userFactory,
+  wageEnhancementFactory,
+} from "@/factories"
 
 import DestroyService from "@/services/centres/destroy-service"
 
@@ -38,6 +61,196 @@ describe("api/src/services/centres/destroy-service.ts", () => {
             data: JSON.stringify(centre),
           }),
         ])
+      })
+
+      test("when centre has building expenses, destroys them with the centre", async () => {
+        // Arrange
+        const currentUser = await userFactory.create()
+        const centre = await centreFactory.create()
+        const fundingPeriod = await fundingPeriodFactory.create({
+          fiscalYear: "2024-2025",
+        })
+        const fiscalPeriod = await fiscalPeriodFactory.create({
+          fundingPeriodId: fundingPeriod.id,
+          fiscalYear: "2024-25",
+        })
+        await buildingExpenseFactory.create({
+          centreId: centre.id,
+          fiscalPeriodId: fiscalPeriod.id,
+        })
+
+        // Act
+        await DestroyService.perform(centre, currentUser)
+
+        // Assert
+        const buildingExpenseCount = await BuildingExpense.count({
+          where: {
+            centreId: centre.id,
+          },
+        })
+        expect(buildingExpenseCount).toEqual(0)
+      })
+
+      test("when centre has employee benefits, destroys them with the centre", async () => {
+        // Arrange
+        const currentUser = await userFactory.create()
+        const centre = await centreFactory.create()
+        const fundingPeriod = await fundingPeriodFactory.create({
+          fiscalYear: "2024-2025",
+        })
+        const fiscalPeriod = await fiscalPeriodFactory.create({
+          fundingPeriodId: fundingPeriod.id,
+          fiscalYear: "2024-25",
+        })
+        await employeeBenefitFactory.create({
+          centreId: centre.id,
+          fiscalPeriodId: fiscalPeriod.id,
+        })
+
+        // Act
+        await DestroyService.perform(centre, currentUser)
+
+        // Assert
+        const employeeBenefitCount = await EmployeeBenefit.count({
+          where: {
+            centreId: centre.id,
+          },
+        })
+        expect(employeeBenefitCount).toEqual(0)
+      })
+
+      test("when centre has funding submission line jsons, destroys them with the centre", async () => {
+        // Arrange
+        const currentUser = await userFactory.create()
+        const centre = await centreFactory.create()
+        await fundingSubmissionLineJsonFactory.create({
+          centreId: centre.id,
+        })
+
+        // Act
+        await DestroyService.perform(centre, currentUser)
+
+        // Assert
+        const fundingSubmissionLineJsonCount = await FundingSubmissionLineJson.count({
+          where: {
+            centreId: centre.id,
+          },
+        })
+        expect(fundingSubmissionLineJsonCount).toEqual(0)
+      })
+
+      test("when centre has payments, destroys them with the centre", async () => {
+        // Arrange
+        const currentUser = await userFactory.create()
+        const centre = await centreFactory.create()
+        const fundingPeriod = await fundingPeriodFactory.create({
+          fiscalYear: "2024-2025",
+        })
+        const fiscalPeriod = await fiscalPeriodFactory.create({
+          fundingPeriodId: fundingPeriod.id,
+          fiscalYear: "2024-25",
+          dateStart: new Date("2024-04-01"),
+          dateEnd: new Date("2024-04-30"),
+        })
+        await paymentFactory.create({
+          centreId: centre.id,
+          fiscalPeriodId: fiscalPeriod.id,
+          paidOn: new Date("2024-04-15").toDateString(),
+        })
+
+        // Act
+        await DestroyService.perform(centre, currentUser)
+
+        // Assert
+        const paymentCount = await Payment.count({
+          where: {
+            centreId: centre.id,
+          },
+        })
+        expect(paymentCount).toEqual(0)
+      })
+
+      test("when centre has wage enhancements, destroys them with the centre", async () => {
+        // Arrange
+        const currentUser = await userFactory.create()
+        const centre = await centreFactory.create()
+        const fundingPeriod = await fundingPeriodFactory.create({
+          fiscalYear: "2024-2025",
+        })
+        const fiscalPeriod = await fiscalPeriodFactory.create({
+          fundingPeriodId: fundingPeriod.id,
+          fiscalYear: "2024-25",
+        })
+        const employeeWageTier = await employeeWageTierFactory.create({
+          fiscalPeriodId: fiscalPeriod.id,
+        })
+        await wageEnhancementFactory.create({
+          centreId: centre.id,
+          employeeWageTierId: employeeWageTier.id,
+        })
+
+        // Act
+        await DestroyService.perform(centre, currentUser)
+
+        // Assert
+        const wageEnhancementCount = await WageEnhancement.count({
+          where: {
+            centreId: centre.id,
+          },
+        })
+        expect(wageEnhancementCount).toEqual(0)
+      })
+
+      test("when centre has funding reconciliations, destroys them with the centre", async () => {
+        // Arrange
+        const currentUser = await userFactory.create()
+        const centre = await centreFactory.create()
+        const fundingPeriod = await fundingPeriodFactory.create({
+          fiscalYear: "2024-2025",
+        })
+        await fundingReconciliationFactory.create({
+          centreId: centre.id,
+          fundingPeriodId: fundingPeriod.id,
+        })
+
+        // Act
+        await DestroyService.perform(centre, currentUser)
+
+        // Assert
+        const fundingReconciliationCount = await FundingReconciliation.count({
+          where: {
+            centreId: centre.id,
+          },
+        })
+        expect(fundingReconciliationCount).toEqual(0)
+      })
+
+      test("when centre has funding reconciliation adjustments, destroys them with the centre", async () => {
+        // Arrange
+        const currentUser = await userFactory.create()
+        const centre = await centreFactory.create()
+        const fundingPeriod = await fundingPeriodFactory.create({
+          fiscalYear: "2024-2025",
+        })
+        const fundingReconciliation = await fundingReconciliationFactory.create({
+          centreId: centre.id,
+          fundingPeriodId: fundingPeriod.id,
+        })
+        const fiscalPeriod = await fiscalPeriodFactory.create({
+          fundingPeriodId: fundingPeriod.id,
+          fiscalYear: "2024-25",
+        })
+        await fundingReconciliationAdjustmentFactory.create({
+          fundingReconciliationId: fundingReconciliation.id,
+          fiscalPeriodId: fiscalPeriod.id,
+        })
+
+        // Act
+        await DestroyService.perform(centre, currentUser)
+
+        // Assert
+        const fundingReconciliationAdjustmentCount = await FundingReconciliationAdjustment.count()
+        expect(fundingReconciliationAdjustmentCount).toEqual(0)
       })
     })
   })
