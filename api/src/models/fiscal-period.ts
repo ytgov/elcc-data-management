2f7ee2e8629be8cdea2be14cdcff2024d8e14f7c
Marlen Brunner <klondikemarlen@gmail.com>
2025-12-23 10:39:39 -0700

    :japanese_castle: Assign explicit table name to fix table name mangling during testing.
    Some table names don't get generated correctly in tests so you need to specify the table name manually. In this case the table name was generating as "_fiscal_periods".
    I believe that this has to do with Vitest/Vite compilation process.

--- api/src/models/fiscal-period.ts
+++ api/src/models/fiscal-period.ts
@@ -14,6 +14,7 @@ import {
   HasMany,
   NotNull,
   PrimaryKey,
+  Table,
   ValidateAttribute,
 } from "@sequelize/core/decorators-legacy"
 import { DateTime } from "luxon"
@@ -57,6 +58,9 @@ export const FISCAL_PERIOD_MONTHS = Object.values<string>(FiscalPeriodMonths)
  *
  * @see FundingPeriod - The parent fiscal year that contains these monthly periods
  */
+@Table({
+  tableName: "fiscal_periods",
+})
 export class FiscalPeriod extends BaseModel<
   InferAttributes<FiscalPeriod>,
   InferCreationAttributes<FiscalPeriod>
