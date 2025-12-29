e87b23fde2b2f8d0504a8afbc3acb2fd349abb77
Marlen Brunner <klondikemarlen@gmail.com>
2025-12-29 09:47:05 -0700

    :recycle: Simplify EnsureDependenciesService; defer to EnsureChildrenService.
    Ensure children better reflects the intent of the service.

--- api/src/services/centres/funding-periods/ensure-dependencies-service.ts
+++ api/src/services/centres/funding-periods/ensure-dependencies-service.ts
@@ -1,22 +1,9 @@
-import { type CreationAttributes } from "@sequelize/core"
-import { isUndefined } from "lodash"
-
-import {
-  BuildingExpense,
-  BuildingExpenseCategory,
-  Centre,
-  FiscalPeriod,
-  FundingPeriod,
-  FundingSubmissionLine,
-  FundingSubmissionLineJson,
-} from "@/models"
+import { Centre, FundingPeriod } from "@/models"
 import BaseService from "@/services/base-service"
-import { FundingPeriods } from "@/services"
+import EnsureChildrenService from "./ensure-children-service"
 
 /**
- * NOTE: this is a shim service until all dependencies are being created by funding-period creation.
- *
- * @deprecated - prefer ensure children type services instead
+ * @deprecated - use EnsureChildrenService instead
  */
 export class EnsureDependenciesService extends BaseService {
   constructor(
@@ -26,117 +13,8 @@ export class EnsureDependenciesService extends BaseService {
     super()
   }
 
-  async perform() {
-    await this.ensureBuildingExpenses()
-    await this.ensureFundingSubmissionLineJsons()
-  }
-
-  private async ensureBuildingExpenses() {
-    const { fiscalYear: fundingPeriodFiscalYear } = this.fundingPeriod
-    const shortFiscalYear = FiscalPeriod.toShortFiscalYearFormat(fundingPeriodFiscalYear)
-
-    const fiscalPeriods = await FiscalPeriod.findAll({
-      attributes: ["id"],
-      where: {
-        fiscalYear: shortFiscalYear,
-      },
-    })
-    if (fiscalPeriods.length === 0) {
-      throw new Error(`No fiscal periods found for fiscal year: ${shortFiscalYear}`)
-    }
-
-    const fiscalPeriodIds = fiscalPeriods.map(({ id }) => id)
-    const existingBuildingExpenseCount = await BuildingExpense.count({
-      where: {
-        centreId: this.centre.id,
-        fiscalPeriodId: fiscalPeriodIds,
-      },
-    })
-    if (existingBuildingExpenseCount > 0) return
-
-    const { fundingRegionId } = this.centre
-    const buildingExpenseCategories = await BuildingExpenseCategory.findAll({
-      include: ["fundingRegion"],
-      where: {
-        fundingRegionId,
-      },
-    })
-    if (buildingExpenseCategories.length === 0) {
-      throw new Error(
-        `No building expense categories found for Centre fundingRegionId="${fundingRegionId}"`
-      )
-    }
-
-    const { buildingUsagePercent } = this.centre
-    const buildingExpensesAttributes: CreationAttributes<BuildingExpense>[] = []
-    for (const fiscalPeriodId of fiscalPeriodIds) {
-      for (const buildingExpenseCategory of buildingExpenseCategories) {
-        const { subsidyRate, fundingRegion } = buildingExpenseCategory
-
-        if (isUndefined(fundingRegion)) {
-          throw new Error("Expected fundingRegion association to be pre-loaded")
-        }
-
-        const { region: fundingRegionSnapshot } = fundingRegion
-
-        buildingExpensesAttributes.push({
-          centreId: this.centre.id,
-          fiscalPeriodId: fiscalPeriodId,
-          buildingExpenseCategoryId: buildingExpenseCategory.id,
-          fundingRegionSnapshot,
-          subsidyRate,
-          buildingUsagePercent,
-          estimatedCost: "0",
-          actualCost: "0",
-          totalCost: "0",
-        })
-      }
-    }
-
-    await BuildingExpense.bulkCreate(buildingExpensesAttributes)
-  }
-
-  private async ensureFundingSubmissionLineJsons() {
-    const { fiscalYear: fundingPeriodFiscalYear } = this.fundingPeriod
-    const fiscalYearLegacy = FundingSubmissionLine.toLegacyFiscalYearFormat(fundingPeriodFiscalYear)
-
-    const fundingSubmissionLineJsonCount = await FundingSubmissionLineJson.count({
-      where: {
-        centreId: this.centre.id,
-        fiscalYear: fiscalYearLegacy,
-      },
-    })
-    if (fundingSubmissionLineJsonCount > 0) return
-
-    const fundingSubmissionLines =
-      await FundingPeriods.FundingSubmissionLines.BulkEnsureService.perform(this.fundingPeriod)
-
-    const defaultLineValues = fundingSubmissionLines.map((fundingSubmissionLine) => ({
-      submissionLineId: fundingSubmissionLine.id,
-      sectionName: fundingSubmissionLine.sectionName,
-      lineName: fundingSubmissionLine.lineName,
-      monthlyAmount: fundingSubmissionLine.monthlyAmount,
-      estimatedChildOccupancyRate: "0",
-      actualChildOccupancyRate: "0",
-      estimatedComputedTotal: "0",
-      actualComputedTotal: "0",
-    }))
-
-    const fundingSubmissionLineJsonAttributes: CreationAttributes<FundingSubmissionLineJson>[] = []
-
-    this.fundingPeriod.forEachMonth((dateStart, dateEnd, monthName) => {
-      const dateName = FundingSubmissionLineJson.asFundingSubmissionLineJsonMonth(monthName)
-      fundingSubmissionLineJsonAttributes.push({
-        centreId: this.centre.id,
-        fiscalYear: fiscalYearLegacy,
-        dateName,
-        dateStart,
-        dateEnd,
-        values: JSON.stringify(defaultLineValues),
-      })
-    })
-
-    await FundingSubmissionLineJson.bulkCreate(fundingSubmissionLineJsonAttributes)
+  async perform(): Promise<void> {
+    return EnsureChildrenService.perform(this.centre, this.fundingPeriod)
   }
 }
 
